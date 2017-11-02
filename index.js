5ab2321bac09eb0be2e4acff5bfa138987a9e3c5
oleonort <oleonortt@gmail.com>
2017-11-02 21:50:31 +0100

    some more algos

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ const playground = (function() {
       console.log(mes);
   }, 500, a);
   document.querySelector('#button').onclick = testDebounce;
+
+  // async map test
+  var job1 = function(cb) {
+    setTimeout(function() { cb('first'); }, 900);
+  };
+
+  var job2 = function(cb) {
+    setTimeout(function() { cb('second'); }, 100);
+  };
+
+  var job3 = function(cb) {
+    setTimeout(function() { cb('third'); }, 300);
+  };
+
+  var jobs = [job1, job2, job3];
+  var callback = function(results) { console.log(results); };
+
+  algorithms.asyncMap(jobs, callback);
 })();
 
 export default playground;
